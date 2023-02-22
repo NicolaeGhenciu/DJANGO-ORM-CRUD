38536b2bd467b329d4b81dee34a5eeecd215f2a7
Ghenciu Nicolae Adrian <114905950+NicolaeGhenciu@users.noreply.github.com>
2023-02-22 09:17:01 +0100

    Add files via upload

--- libreria/urls.py
+++ libreria/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path('like/<int:id_libro>', views.like, name="like"),
     path('libros/<int:id_libro>/comentar', views.comentar, name="comentar"),
     path('comentarios/<int:id_libro>', views.ver_comentarios, name='ver_comentarios'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- libreria/views.py
+++ libreria/views.py
@@ -5,22 +5,54 @@ from .forms import LibroForm
 from .models import Comentario
 from django.utils import timezone
 
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
+def is_authenticated(request):
+    return request.user.is_authenticated
 
+@login_required(login_url='/login/')
 def inicio(request):
     return render(request, 'paginas/inicio.html')
 
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request=request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('libros')
+            else:
+                messages.error(request, 'Invalid username or password')
+        else:
+            messages.error(request, 'Invalid username or password')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
 
+def logout_view(request):
+    logout(request)
+    return redirect('/login')
+
+@login_required(login_url='/login/')
 def nosotros(request):
     return render(request, 'paginas/nosotros.html')
 
-
+@login_required(login_url='/login/')
 def libros(request):
     libros = Libro.objects.all()
     return render(request, 'libros/index.html', {'libros': libros})
 
-
+@login_required(login_url='/login/')
 def crear(request):
     formulario = LibroForm(request.POST or None, request.FILES or None)
     if formulario.is_valid():
@@ -28,7 +60,7 @@ def crear(request):
         return redirect('libros')
     return render(request, 'libros/crear.html', {'formulario': formulario})
 
-
+@login_required(login_url='/login/')
 def editar(request, id):
     libro = Libro.objects.get(id=id)
     formulario = LibroForm(request.POST or None,
@@ -38,20 +70,20 @@ def editar(request, id):
         return redirect('libros')
     return render(request, 'libros/editar.html', {'formulario': formulario})
 
-
+@login_required(login_url='/login/')
 def eliminar(request, id):
     libro = Libro.objects.get(id=id)
     libro.delete()
     return redirect('libros')
 
-
+@login_required(login_url='/login/')
 def like(request, id_libro):
     libro = Libro.objects.get(id=id_libro)
     libro.likes += 1
     libro.save()
     return redirect('libros')
 
-
+@login_required(login_url='/login/')
 def comentar(request, id_libro):
     libro = Libro.objects.get(id=id_libro)
     comentarios = Comentario.objects.filter(libro=libro)
@@ -64,7 +96,8 @@ def comentar(request, id_libro):
         c.save()
         return redirect('libros')
     return render(request, 'libros/comentar.html', {'libro': libro, 'comentarios': comentarios})
-
+    
+@login_required(login_url='/login/')
 def ver_comentarios(request, id_libro):
     libro = Libro.objects.get(id=id_libro)
     comentarios = Comentario.objects.filter(libro=libro)
